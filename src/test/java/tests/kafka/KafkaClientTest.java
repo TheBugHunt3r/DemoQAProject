b79e34408ff0cffb5b4a9686b934168a53107570
alisapavluchenkova <sonyapvl666@gmail.com>
2026-09-22 00:02:25 +0300

    fix(kafka): fix message flow test by sending message before consumer polling

--- src/test/java/tests/kafka/KafkaClientTest.java
+++ src/test/java/tests/kafka/KafkaClientTest.java
@@ -36,19 +36,21 @@ public class KafkaClientTest {
             "асинхронное получение через Consumer (Awaitility).")
     public void testKafkaMessageFlow() {
         String expectedMessage = "{\"status\": \"SUCCESS\", \"test\": \"LoginTest\"}";
+        producer.sendMessage(TOPIC_NAME, "user_123", expectedMessage);
+        System.out.println(">>> Сообщение отправлено ДО ожидания консьюмера");
         Awaitility.await()
                 .atMost(45, TimeUnit.SECONDS)
                 .pollInterval(Duration.ofMillis(100))
                 .until(consumer::hasAssignedPartitions);
-        producer.sendMessage(TOPIC_NAME, "user_123", expectedMessage);
         Awaitility.await()
                 .atMost(30, TimeUnit.SECONDS)
-                .pollInterval(Duration.ofMillis(500))
+                .pollInterval(Duration.ofSeconds(1))
                 .untilAsserted(() -> {
-                    ConsumerRecords<String, String> records = consumer.pollMessages(Duration.ofMillis(200));
+                    ConsumerRecords<String, String> records = consumer.pollMessages(Duration.ofSeconds(2));
                     boolean found = false;
                     System.out.println(">>> Polled records count: " + records.count());
                     for (ConsumerRecord<String, String> record : records) {
+                        System.out.println(">>> Прочитано из Kafka -> Значение: " + record.value());
                         if (record.value() != null && record.value().contains("SUCCESS")) {
                             found = true;
                             break;
